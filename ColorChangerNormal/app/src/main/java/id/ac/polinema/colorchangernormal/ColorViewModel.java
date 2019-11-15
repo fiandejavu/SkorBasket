025ee2173aedda6fdb412e1d30b1390286bfd834
fiandejavu <viandejavu@gmail.com>
2019-11-15 16:27:25 +0700

    Percobaan 2

---
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/ColorViewModel.java
@@ -0,0 +1,29 @@
+package id.ac.polinema.colorchangernormal;
+
+import android.app.Application;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ColorViewModel extends AndroidViewModel {
+
+    private int color = 0xfff;
+
+    public ColorViewModel(@NonNull Application application) {
+        super(application);
+        ConstraintLayout rootView;
+        Button btnChangeColor;
+        // Tambahkan ViewModel
+        ColorViewModel colorViewModel;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
